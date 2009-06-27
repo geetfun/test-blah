36d3d8ca189e71e974e47b5abdb8e213b3c84ffe
Simon Chiu <skhchiu@gmail.com>
2009-06-27 12:32:10 -0400

    added configuration files

--- models/sequel/connection.rb
+++ models/sequel/connection.rb
@@ -1,4 +1,4 @@
 require 'sequel'
 
 Sequel::Model.raise_on_save_failure = false
-DB = Sequel.connect('postgres://postgres:123moose@localhost:5432/scalerUsers_development')
+DB = Sequel.connect("postgres://postgres:123moose@localhost:5432/#{Sinatra::Application.current_db}")
